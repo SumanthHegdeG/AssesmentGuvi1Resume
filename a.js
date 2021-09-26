5074f8815bbf9f9ae8e976efc146a54cc9bd7b35
unknown <you@example.com>
2021-09-26 13:29:55 +0530

    First commit

---
+++ a.js
@@ -0,0 +1,5 @@
+function foo(){
+var result = document.getElementById("test").value;
+console.log(result)
+}
+
---
+++ day2.js
@@ -0,0 +1,24 @@
+//day 2 assesment
+
+console.log(typeof(1));
+console.log(typeof(1.1));
+console.log(typeof('1.1'));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof([]));
+console.log(typeof({}));
+console.log(typeof(NaN));
+
+// solution
+/*
+a.js:24 number
+a.js:25 number
+a.js:26 string
+a.js:27 boolean
+a.js:28 object
+a.js:29 undefined
+a.js:30 object
+a.js:31 object
+a.js:32 number
+*/
